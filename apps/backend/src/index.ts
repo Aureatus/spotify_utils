f2f5fa72920444785eb5892e54542de41e382924
Alex Knott <alextknott@gmail.com>
2025-02-11 19:16:04 +0000

    Add CORS to elysia, allowing all urls

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().get("/health", () => "OK").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .get("/health", () => "OK")
+  .listen(3000);
 
 console.log(
   `🦊 Backend running at ${app.server?.hostname}:${app.server?.port}`
