4cd6ac08e11b56229102ed0d4f1aed5918c28ccf
Alex Knott <alextknott@gmail.com>
2025-02-13 07:53:04 +0000

    provide schema type to drizzle orm constructor

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -1,9 +1,10 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
+import * as schema from "./schema";
 
 console.log(process.env.DB_FILE_NAME!);
 
 const sqlite = new Database(process.env.DB_FILE_NAME!);
 
-export const db = drizzle(sqlite);
+export const db = drizzle<typeof schema>(sqlite);
