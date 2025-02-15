f31158527d0ea6231c3650ef37bf570f1d4bc78c
Alex <93552500+Aureatus@users.noreply.github.com>
2025-02-15 13:38:50 +0000

    Instantiate and export elysia treaty client (#8)

---
+++ apps/frontend/src/lib/elysia-client.ts
@@ -0,0 +1,6 @@
+import { treaty } from "@elysiajs/eden";
+import type { App } from "../../../backend/src/index";
+
+export const app = treaty<App>("http://localhost:3000", {
+  fetch: { credentials: "include" },
+});
