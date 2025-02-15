b742ffb3d401a2e1b0f000fcdf1182da8d19f4bc
Alex Knott <alextknott@gmail.com>
2025-02-15 13:50:10 +0000

    Add get route to get spotify playlists for current user based on access_token

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,11 +3,15 @@ import cors from "@elysiajs/cors";
 
 import betterAuthView from "./utils/auth/view";
 import { userMiddleware } from "./middlewares/auth-middleware";
+import { getCurrentUserPlaylists } from "./lib/spotify";
 
 const app = new Elysia()
   .use(cors())
   .derive(({ request }) => userMiddleware(request))
   .all("/api/auth/*", betterAuthView)
+  .get("/api/spotify/playlists", async ({ access_token }) => {
+    return access_token && (await getCurrentUserPlaylists(access_token));
+  })
   .get("/health", () => "OK")
   .listen(3000);
 
