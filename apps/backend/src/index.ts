65ac0ea1af1baceec9836475ef7810bef85123d8
Alex Knott <alextknott@gmail.com>
2025-02-16 16:57:22 +0000

    Create group for routes with api prefix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -11,35 +11,39 @@ import {
 const app = new Elysia()
   .use(cors())
   .derive(({ request }) => userMiddleware(request))
-  .all("/api/auth/*", betterAuthView)
-  .get("/api/spotify/playlists", async ({ access_token }) => {
-    return access_token && (await getCurrentUserPlaylists(access_token));
-  })
-  .post(
-    "/api/spotify/merge",
-    async ({ access_token, body }) => {
-      return (
-        access_token &&
-        (await createMergedPlaylistForUser(
-          access_token,
-          body.playlistName,
-          body.playlistsToMerge
-        ))
-      );
-    },
-    {
-      body: t.Object({
-        playlistName: t.String(),
-        playlistsToMerge: t.Array(
-          t.Object({
-            name: t.String(),
-            id: t.String(),
-            trackListCount: t.Number(),
-          })
-        ),
-      }),
-    }
+  .group("/api", (app) =>
+    app
+      .all("/auth/*", betterAuthView)
+      .get("/spotify/playlists", async ({ access_token }) => {
+        return access_token && (await getCurrentUserPlaylists(access_token));
+      })
+      .post(
+        "/spotify/merge",
+        async ({ access_token, body }) => {
+          return (
+            access_token &&
+            (await createMergedPlaylistForUser(
+              access_token,
+              body.playlistName,
+              body.playlistsToMerge
+            ))
+          );
+        },
+        {
+          body: t.Object({
+            playlistName: t.String(),
+            playlistsToMerge: t.Array(
+              t.Object({
+                name: t.String(),
+                id: t.String(),
+                trackListCount: t.Number(),
+              })
+            ),
+          }),
+        }
+      )
   )
+
   .get("/health", () => "OK")
   .listen(3000);
 
