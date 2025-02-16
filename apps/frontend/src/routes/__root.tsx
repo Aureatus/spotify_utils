28f982e4684cdb49df5ecb010d1c2e6131be9fd9
Alex Knott <alextknott@gmail.com>
2025-02-16 11:59:05 +0000

    Configure shadcn toast

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -6,13 +6,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import queryClient from "@/lib/query-client";
 
+import { Toaster } from "@/components/ui/toaster";
+
 export const Route = createRootRoute({
   component: () => {
     return (
       <QueryClientProvider client={queryClient}>
         <Outlet />
-        <TanStackRouterDevtools />
-        <ReactQueryDevtools initialIsOpen={false} />
+        <TanStackRouterDevtools position="top-left" />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-left"
+        />
+        <Toaster />
       </QueryClientProvider>
     );
   },
