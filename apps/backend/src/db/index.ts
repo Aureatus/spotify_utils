17067291320be88c0a91362b08aaa1e76036b693
Alex Knott <alextknott@gmail.com>
2025-02-17 19:45:30 +0000

    Set sqlite to WAL mode

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -5,5 +5,6 @@ import { DB_FILE_NAME } from "../env";
 import * as schema from "./schema";
 
 const sqlite = new Database(DB_FILE_NAME);
+sqlite.exec("PRAGMA journal_mode = WAL;");
 
 export const db = drizzle(sqlite, { schema: schema });
